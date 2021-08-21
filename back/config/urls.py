941e322d1453a7e4a8a31cd308293583e9c4451c
hyunmin0317 <choihm9903@naver.com>
2021-08-21 15:47:42 +0900

    Update back-singup

---
+++ back/config/urls.py
@@ -0,0 +1,9 @@
+# mysite/urls.py
+from django.urls import path, include
+from django.contrib import admin
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("api/auth", include("knox.urls")),
+]
